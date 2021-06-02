c3d9f1ff50c8afb046088582e587ecad04dd5800
DylanB95EST <dylan.byrne@est.tech>
2021-06-02 19:45:46 +0100

    Adding new sub repo and modules, along with rest endpoint, test case and Spring Application class
    Issue-ID: CPS-431
    
    Change-Id: I6fd4379df6133108c1f8f2a5339f990f16773ad6
    Signed-off-by: DylanB95EST <dylan.byrne@est.tech>

---
+++ dmi-rest/src/test/groovy/org/onap/cps/rest/controller/DmiRestControllerSpec.groovy
@@ -0,0 +1,53 @@
+/*
+ *  ============LICENSE_START=======================================================
+ *  Copyright (C) 2021 Nordix Foundation
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.rest.controller
+
+import org.springframework.http.HttpStatus
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.web.servlet.MockMvc
+import spock.lang.Specification
+
+@WebMvcTest
+class DmiRestControllerSpec extends Specification{
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Value('${rest.api.dmi-base-path}')
+    def basePath
+
+    def 'Get Hello World'() {
+        given: 'hello world endpoint'
+        def helloWorldEndpoint = "$basePath/v1/helloworld"
+
+        when: 'get hello world api is invoked'
+        def response = mvc.perform(get(helloWorldEndpoint)).andReturn().response
+
+        then: 'Hello World is Returned'
+        response.status == HttpStatus.OK.value()
+        response.getContentAsString().contains("Hello World")
+    }
+
+}
