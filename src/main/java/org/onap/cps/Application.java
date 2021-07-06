6fe917c984f86e6e78af5a1acf1be8b42c5f7703
niamhcore <niamh.core@est.tech>
2021-07-06 10:32:17 +0100

    Implement Plugin Registration in DMI-Plugin
    Issue-ID: CPS-405
    Signed-off-by: niamhcore <niamh.core@est.tech>
    Change-Id: I0a7ab9bb0ca3861b171388613c44f7bc6e7f9aa9

--- src/main/java/org/onap/cps/Application.java
+++ src/main/java/org/onap/cps/Application.java
@@ -18,7 +18,7 @@
  * ============LICENSE_END=========================================================
  */
 
-package org.onap.cps.ncmp;
+package org.onap.cps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
