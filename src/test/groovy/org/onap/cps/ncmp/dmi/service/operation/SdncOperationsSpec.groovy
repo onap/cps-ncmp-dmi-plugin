0f542013571855f2d48981b1a2a124650ab204af
tragait <rahul.tyagi@est.tech>
2021-09-14 13:47:52 +0100

    fix data from object to string
    Issue-ID: CPS-634
    Signed-off-by: tragait <rahul.tyagi@est.tech>
    Change-Id: I6fe794b3e65b6a3aae2a1698ea64a925238a18d3

--- src/test/groovy/org/onap/cps/ncmp/dmi/service/operation/SdncOperationsSpec.groovy
+++ src/test/groovy/org/onap/cps/ncmp/dmi/service/operation/SdncOperationsSpec.groovy
@@ -73,7 +73,7 @@ class SdncOperationsSpec extends Specification {
         given: 'excpected url, topology-id, sdncOperation object'
             def expectedUrl = '/rests/data/network-topology:network-topology/topology=test-topology/node=node1/yang-ext:mount/testResourceId'
         when: 'write resource data for pass through running is called'
-            objectUnderTest.writeResourceDataPassthroughRunning('node1', 'testResourceId', 'application/json','testData')
+            objectUnderTest.writeResourceDataPassthroughRunning('node1', 'testResourceId', 'application/json','{ "some-data": "some-value" }')
         then: 'the post operation is executed with the correct URL'
             1 * mockSdncRestClient.postOperationWithJsonData(expectedUrl, _ as String, _ as HttpHeaders)
     }
