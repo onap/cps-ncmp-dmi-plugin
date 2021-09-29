bf03a5e85737c6ecf898b04036b1f071d8266dc2
niamhcore <niamh.core@est.tech>
2021-09-29 15:32:32 +0100

    Changing resource identifier to a query param
    * Updating yaml
    * Update jib-maven-plugin version
    
    Issue-ID: CPS-679
    Signed-off-by: niamhcore <niamh.core@est.tech>
    Change-Id: Ie2f49bd8e22723ca4dcd2c20ed39722f22c281c1

--- src/test/groovy/org/onap/cps/ncmp/dmi/rest/controller/DmiRestControllerSpec.groovy
+++ src/test/groovy/org/onap/cps/ncmp/dmi/rest/controller/DmiRestControllerSpec.groovy
@@ -187,7 +187,7 @@ class DmiRestControllerSpec extends Specification {
     def 'Get resource data for pass-through operational from cm handle.'() {
         given: 'Get resource data url'
             def getResourceDataForCmHandleUrl = "${basePathV1}/ch/some-cmHandle/data/ds/ncmp-datastore:passthrough-operational" +
-                    "/resourceIdentifier?fields=myfields&depth=5"
+                    "?resourceIdentifier=abc/xyz&fields=myfields&depth=5"
             def json = '{"cmHandleProperties" : { "prop1" : "value1", "prop2" : "value2"}}'
         when: 'get resource data PUT api is invoked'
             def response = mvc.perform(
@@ -198,7 +198,7 @@ class DmiRestControllerSpec extends Specification {
             response.status == HttpStatus.OK.value()
         and: 'dmi service called with get resource data for cm handle'
             1 * mockDmiService.getResourceDataOperationalForCmHandle('some-cmHandle',
-                    'resourceIdentifier',
+                    'abc/xyz',
                     'application/json',
                     'myfields',
                     5,
@@ -207,7 +207,8 @@ class DmiRestControllerSpec extends Specification {
 
     def 'Write data using passthrough running for a cm handle using #scenario.'() {
         given: 'write data for cmHandle url and jsonData'
-            def writeDataforCmHandlePassthroughRunning = "${basePathV1}/ch/some-cmHandle/data/ds/ncmp-datastore:passthrough-running/some-resourceIdentifier"
+            def writeDataforCmHandlePassthroughRunning = "${basePathV1}/ch/some-cmHandle/data/ds/ncmp-datastore:passthrough-running" +
+                    "?resourceIdentifier=some-resourceIdentifier"
             def jsonData = TestUtils.getResourceFileContent(requestBodyFile)
         and: 'dmi service is called'
             mockDmiService.writeResourceDataPassthroughForCmHandle('some-cmHandle',
@@ -231,7 +232,7 @@ class DmiRestControllerSpec extends Specification {
     def 'Get resource data for pass-through running from cm handle.'() {
         given: 'Get resource data url'
             def getResourceDataForCmHandleUrl = "${basePathV1}/ch/some-cmHandle/data/ds/ncmp-datastore:passthrough-running" +
-                     "/testResourceIdentifier?fields=testFields&depth=5"
+                    "?resourceIdentifier=testResourceIdentifier&fields=testFields&depth=5"
             def json = '{"cmHandleProperties" : { "prop1" : "value1", "prop2" : "value2"}}'
         when: 'get resource data PUT api is invoked'
             def response = mvc.perform(
