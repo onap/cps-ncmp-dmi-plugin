1d67985ccee400564cbc141ddf83452be0f505b7
halil.cakal <halil.cakal@est.tech>
2023-02-22 12:53:45 +0000

    Add Kafka Producer
    - add test for producer
    
    Issue-ID: CPS-1505
    Change-Id: I932568b47431d437bd212e15934b40d4f13b1c5b
    Signed-off-by: halil.cakal <halil.cakal@est.tech>

---
+++ src/test/groovy/org/onap/cps/ncmp/dmi/notifications/avc/SubscriptionEventResponseProducerServiceSpec.groovy
@@ -0,0 +1,42 @@
+/*
+ * ============LICENSE_START=======================================================
+ * Copyright (C) 2023 Nordix Foundation
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.ncmp.dmi.notifications.avc
+
+import org.onap.cps.ncmp.event.model.Payload
+import org.onap.cps.ncmp.event.model.ResponseData
+import spock.lang.Specification
+
+class SubscriptionEventResponseProducerServiceSpec extends Specification {
+
+    def mockSubscriptionEventResponseProducer = Mock(SubscriptionEventResponseProducer)
+
+    def objectUnderTest = new SubscriptionEventResponseProducerService(mockSubscriptionEventResponseProducer)
+
+    def 'Create and publish subscription response event successfully'() {
+        given: 'a message key and a message value'
+            def messageKey = UUID.randomUUID().toString()
+            def messageValue = new ResponseData(payload: new Payload())
+        when: 'service is called to publish subscription response data'
+            objectUnderTest.publishSubscriptionEventResponse(messageKey, messageValue)
+        then: 'producer is called one time'
+            1 * mockSubscriptionEventResponseProducer.publishResponseEventMessage(_, messageKey, messageValue)
+    }
+}
