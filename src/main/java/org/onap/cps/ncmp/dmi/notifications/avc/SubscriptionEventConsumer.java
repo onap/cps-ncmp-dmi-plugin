d2a72a931e6a5ab6098277ed038dff6d74254095
halil.cakal <halil.cakal@est.tech>
2023-03-14 11:24:26 +0000

    Subscription Create Response Handling Dmi Part
    - Consume suscription event from ncmp.
    - Generate subscription event response and revert back to ncmp to
      specified topic.
    - Configure kafka consumer properties.
    
    Issue-ID: CPS-1492
    Change-Id: I87de30c00e0f93e350ce6f9fd6079504952da09b
    Signed-off-by: halil.cakal <halil.cakal@est.tech>

---
+++ src/main/java/org/onap/cps/ncmp/dmi/notifications/avc/SubscriptionEventConsumer.java
@@ -0,0 +1,70 @@
+/*
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2023 Nordix Foundation
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.ncmp.dmi.notifications.avc;
+
+import java.util.ArrayList;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.onap.cps.ncmp.dmi.service.model.SubscriptionEventResponse;
+import org.onap.cps.ncmp.dmi.service.model.SubscriptionEventResponseStatus;
+import org.onap.cps.ncmp.event.model.SubscriptionEvent;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class SubscriptionEventConsumer {
+
+    @Value("${app.dmi.avc.subscription-response-topic}")
+    private String cmAvcSubscriptionResponseTopic;
+    private final KafkaTemplate<String, SubscriptionEventResponse> kafkaTemplate;
+
+    /**
+     * Sends message to the configured topic.
+     *
+     * @param subscriptionEventResponse is the payload of the kafka message
+     */
+    public void sendSubscriptionResponseMessage(final SubscriptionEventResponse subscriptionEventResponse) {
+        kafkaTemplate.send(cmAvcSubscriptionResponseTopic, subscriptionEventResponse);
+    }
+
+    /**
+     * Consume the specified event.
+     *
+     * @param subscriptionEvent the event to be consumed
+     */
+    @KafkaListener(topics = "${app.dmi.avc.subscription-topic}",
+            properties = {"spring.json.value.default.type=org.onap.cps.ncmp.event.model.SubscriptionEvent"})
+    public void consumeSubscriptionEvent(final SubscriptionEvent subscriptionEvent) {
+        final String clientId = subscriptionEvent.getEvent().getSubscription().getClientID();
+        log.info("Subscription for ClientID {} is received by dmi plugin.", clientId);
+        final ArrayList<String> cmHandleIds = (ArrayList<String>) subscriptionEvent.getEvent().getPredicates().get(
+                "targets");
+        final SubscriptionEventResponse subscriptionEventResponse = new SubscriptionEventResponse();
+        subscriptionEventResponse.setClientId(clientId);
+        subscriptionEventResponse.setStatus(SubscriptionEventResponseStatus.ACCEPTED.getResponseStatus());
+        subscriptionEventResponse.setCmHandleIds(cmHandleIds);
+        sendSubscriptionResponseMessage(subscriptionEventResponse);
+    }
+}
