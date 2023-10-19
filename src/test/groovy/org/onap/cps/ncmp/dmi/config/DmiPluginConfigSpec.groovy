f3eed0b90086708c1cff02b99ea64ff2371f564e
egernug <gerard.nugent@est.tech>
2023-10-19 11:23:13 +0100

    Upgrade of DMI-Plugin to Spring Boot 3.1.2
    Issue-ID: CPS-1790
    
    Signed-off-by: egernug <gerard.nugent@est.tech>
    Change-Id: I3eaf8307660ce8d8d33ad98cabb891bcdd663713

--- src/test/groovy/org/onap/cps/ncmp/dmi/config/DmiPluginConfigSpec.groovy
+++ src/test/groovy/org/onap/cps/ncmp/dmi/config/DmiPluginConfigSpec.groovy
@@ -1,6 +1,6 @@
 /*
  *  ============LICENSE_START=======================================================
- *  Copyright (C) 2021-2022 Nordix Foundation
+ *  Copyright (C) 2021-2023 Nordix Foundation
  *  ================================================================================
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -20,7 +20,7 @@
 
 package org.onap.cps.ncmp.dmi.config
 
-import org.springdoc.core.GroupedOpenApi
+import org.springdoc.core.models.GroupedOpenApi
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ContextConfiguration
