0e2f59acc41115d91132d6e772e3839480fba203
seanbeirne <sean.beirne@est.tech>
2024-03-12 12:07:54 +0000

    Read and send Response (DMI <> NCMP)
    -Update subscription code to new models
    
    Issue-ID: CPS-1971
    Change-Id: I382ca31407f8088ddea889a7ab904a22c09789ff
    Signed-off-by: seanbeirne <sean.beirne@est.tech>

---
+++ src/main/java/org/onap/cps/ncmp/dmi/notifications/cmsubscription/CmNotificationSubscriptionDmiOutEventToCloudEventMapper.java
@@ -0,0 +1,63 @@
+/*
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2024 Nordix Foundation
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.ncmp.dmi.notifications.cmsubscription;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.cloudevents.CloudEvent;
+import io.cloudevents.core.builder.CloudEventBuilder;
+import java.net.URI;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.onap.cps.ncmp.dmi.exception.CloudEventConstructionException;
+import org.onap.cps.ncmp.events.cmnotificationsubscription_merge1_0_0.dmi_to_ncmp.CmNotificationSubscriptionDmiOutEvent;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class CmNotificationSubscriptionDmiOutEventToCloudEventMapper {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    /**
+     * Maps SubscriptionEventResponse to a CloudEvent.
+     *
+     * @param cmSubscriptionDmiOutEvent object.
+     * @param subscriptionType          String of subscription type.
+     * @param dmiName                   String of dmiName.
+     * @param correlationId             String of correlationId.
+     * @return CloudEvent built.
+     */
+    public static CloudEvent toCloudEvent(final CmNotificationSubscriptionDmiOutEvent cmSubscriptionDmiOutEvent,
+                                          final String subscriptionType, final String dmiName,
+                                          final String correlationId) {
+        try {
+            return CloudEventBuilder.v1().withId(UUID.randomUUID().toString()).withSource(URI.create(dmiName))
+                .withType(subscriptionType)
+                .withDataSchema(URI.create("urn:cps:" + CmNotificationSubscriptionDmiOutEvent.class.getName()
+                    + ":1.0.0"))
+                .withExtension("correlationid", correlationId)
+                .withData(objectMapper.writeValueAsBytes(cmSubscriptionDmiOutEvent)).build();
+        } catch (final Exception ex) {
+            throw new CloudEventConstructionException("The Cloud Event could not be constructed",
+                "Invalid object to serialize or required headers is missing", ex);
+        }
+    }
+
+}
