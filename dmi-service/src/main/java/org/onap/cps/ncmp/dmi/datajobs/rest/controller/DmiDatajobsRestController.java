658f4d4786b7f7290d91d3d16e99c405c176d686
leventecsanyi <levente.csanyi@est.tech>
2024-08-05 15:46:59 +0200

    Fixed URL for data job result
    - fixed opeanapi and stub
    - updated pnp simulator image
    
    Issue-ID: CPS-2296
    Signed-off-by: leventecsanyi <levente.csanyi@est.tech>
    Change-Id: I6654762256097f9deb8ce40cfc8014bed67cb269

--- dmi-service/src/main/java/org/onap/cps/ncmp/dmi/datajobs/rest/controller/DmiDatajobsRestController.java
+++ dmi-service/src/main/java/org/onap/cps/ncmp/dmi/datajobs/rest/controller/DmiDatajobsRestController.java
@@ -70,15 +70,14 @@ public class DmiDatajobsRestController implements DmiDatajobApi {
     /**
      * This method is not implemented for ONAP DMI plugin.
      *
-     * @param dataProducerJobId     Identifier for the data producer job (required)
      * @param dataProducerId        Identifier for the data producer as a query parameter (required)
+     * @param dataProducerJobId     Identifier for the data producer job (required)
      * @param destination           The destination of the results, Kafka topic name or s3 bucket name (required)
      * @return ResponseEntity       Response entity indicating the method is not implemented
      */
     @Override
-    public ResponseEntity<Void> getDataJobResult(final String requestId,
+    public ResponseEntity<Void> getDataJobResult(final String dataProducerId,
                                                  final String dataProducerJobId,
-                                                 final String dataProducerId,
                                                  final String destination) {
         return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
     }
