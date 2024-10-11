6be5a6702d1b1bc23f90cdf862d605271aee16e3
halil.cakal <halil.cakal@est.tech>
2024-10-11 17:22:06 +0100

    DMI simulator serving yang modules dynamically
    - all modules and module references are created once,
       then are served as requested
    - 5 module-set-tag supported from tagA to tagE
    - 200 yang modules per module-set-tag
    - module sizes are fixed at 32KiB each
    - the average response size is 200 * 32KiB = 6.4MiB
       for each
    
    Issue-ID: CPS-2410
    
    Change-Id: Ie7ac73b8c0bd464b114a8d76104db4b569ae36f4
    Signed-off-by: halil.cakal <halil.cakal@est.tech>

--- dmi-stub/dmi-plugin-demo-and-csit-stub-app/src/main/java/org/onap/cps/ncmp/dmi/rest/stub/DmiDemoApplication.java
+++ dmi-stub/dmi-plugin-demo-and-csit-stub-app/src/main/java/org/onap/cps/ncmp/dmi/rest/stub/DmiDemoApplication.java
@@ -1,6 +1,6 @@
 /*
  * ============LICENSE_START=======================================================
- *  Copyright (C) 2023 Nordix Foundation
+ *  Copyright (C) 2023-2024 Nordix Foundation
  *  ================================================================================
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -18,7 +18,7 @@
  *  ============LICENSE_END=========================================================
  */
 
-package org.onap.cps.ncmp.dmi.rest.stub.controller;
+package org.onap.cps.ncmp.dmi.rest.stub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
