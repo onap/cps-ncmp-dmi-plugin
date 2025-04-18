b8d0f858a955ef62d25565298b576cd4482f932b
mpriyank <priyank.maheshwari@est.tech>
2025-04-18 11:54:27 +0100

    [DMI] Automate the process of device registration #1
    - read from agreed topic using the VesEventSchema and call the
      registration endpoint in cps-ncmp to automate the device registration
      during mounting of the device.
    - also added non cloud kafka consumer factory as VES event is not cloud
      compliant
    - testware added
    
    Issue-ID: CPS-2710
    Change-Id: I5ab695afc225dcc372cff00a2f6f69c9047b14ed
    Signed-off-by: mpriyank <priyank.maheshwari@est.tech>

---
+++ dmi-service/src/test/groovy/org/onap/cps/ncmp/dmi/cmstack/ves/VesEventConsumerSpec.groovy
@@ -0,0 +1,98 @@
+/*
+ * ============LICENSE_START========================================================
+ *  Copyright (c) 2025 OpenInfra Foundation Europe. All rights reserved.
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.ncmp.dmi.cmstack.ves
+
+import ch.qos.logback.classic.Level
+import ch.qos.logback.classic.Logger
+import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.core.read.ListAppender
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.onap.cps.ncmp.dmi.TestUtils
+import org.onap.cps.ncmp.dmi.api.kafka.MessagingBaseSpec
+import org.onap.cps.ncmp.dmi.exception.CmHandleRegistrationException
+import org.onap.cps.ncmp.dmi.service.DmiService
+import org.onap.cps.ncmp.events.ves30_2_1.VesEventSchema
+import org.slf4j.LoggerFactory
+import org.spockframework.spring.SpringBean
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.annotation.DirtiesContext
+import org.testcontainers.spock.Testcontainers
+
+@SpringBootTest(classes = [ObjectMapper])
+@Testcontainers
+@DirtiesContext
+class VesEventConsumerSpec extends MessagingBaseSpec {
+
+    def objectMapper = new ObjectMapper()
+    def dmiService = Mock(DmiService)
+
+    @SpringBean
+    VesEventConsumer objectUnderTest = new VesEventConsumer(dmiService)
+
+    def logger = Spy(ListAppender<ILoggingEvent>)
+
+    def vesEvent
+
+    void setup() {
+
+        def jsonData = TestUtils.getResourceFileContent('sampleVesEvent.json')
+        vesEvent = objectMapper.readValue(jsonData, VesEventSchema.class)
+
+        ((Logger) LoggerFactory.getLogger(VesEventConsumer.class)).addAppender(logger)
+        logger.start()
+    }
+
+    void cleanup() {
+        ((Logger) LoggerFactory.getLogger(VesEventConsumer.class)).detachAndStopAllAppenders()
+    }
+
+
+    def 'Consume a VES event'() {
+        when: 'event is consumed'
+            objectUnderTest.consumeVesEvent(vesEvent)
+        then: 'the dmi service is registered correctly'
+            1 * dmiService.registerCmHandles(['pynts-o-du-o1'])
+
+    }
+
+    def 'Consume create event for existing cm handle'() {
+        given: 'the cm handle is already registered with NCMP'
+            dmiService.registerCmHandles(_) >> { throw new CmHandleRegistrationException('the cm handle already exists') }
+        when: 'event is consumed'
+            objectUnderTest.consumeVesEvent(vesEvent)
+        then: 'the correct exception is logged as a warning'
+            def loggingEvent = logger.list[1]
+            assert loggingEvent.level == Level.WARN
+            assert loggingEvent.formattedMessage.contains('Exception occurred while registering the device : pynts-o-du-o1')
+    }
+
+    def 'Consume a VES message but cm handle registration results in exception'() {
+        given: 'Exception occurs when registering cm handle with NCMP'
+            dmiService.registerCmHandles(_) >> { throw new Exception('unknown exception occurred') }
+        when: 'event is consumed'
+            objectUnderTest.consumeVesEvent(vesEvent)
+        then: 'the correct exception is logged as a warning'
+            def loggingEvent = logger.list[1]
+            assert loggingEvent.level == Level.WARN
+            assert loggingEvent.formattedMessage.contains('Exception occurred')
+    }
+
+}
