e6ea7ffac851fcbc94aa880ad878fca35e16188a
mpriyank <priyank.maheshwari@est.tech>
2025-04-18 11:54:27 +0100

    [DMI] Automate the process of device registration #1
    - read from agreed topic using the VesEventSchema and call the
      registration endpoint in cps-ncmp to automate the device registration
      during mounting of the device.
    - also added non cloud kafka consumer factory as VES event is not cloud
      compliant
    - testware added
    
    Issue-ID: CPS-2710
    Change-Id: I5ab695afc225dcc372cff00a2f6f69c9047b14ed
    Signed-off-by: mpriyank <priyank.maheshwari@est.tech>

---
+++ dmi-service/src/test/groovy/org/onap/cps/ncmp/dmi/cmstack/ves/VesEventsConfigurationSpec.groovy
@@ -0,0 +1,45 @@
+/*
+ * ============LICENSE_START========================================================
+ *  Copyright (c) 2025 OpenInfra Foundation Europe. All rights reserved.
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.ncmp.dmi.cmstack.ves
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ContextConfiguration
+import spock.lang.Specification
+
+@SpringBootTest
+@ContextConfiguration(classes = [VesEventsConfiguration])
+@EnableConfigurationProperties
+class VesEventsConfigurationSpec extends Specification {
+
+    @Autowired
+    VesEventsConfiguration vesEventsConfiguration
+
+    def 'Check the test topics configured'() {
+        expect: 'VES topics are populated'
+            assert vesEventsConfiguration.topicNames.size() == 2
+        and: 'correct topics are present'
+            assert vesEventsConfiguration.topicNames.contains('unauthenticated.VES_PNFREG_OUTPUT')
+            assert vesEventsConfiguration.topicNames.contains('unauthenticated.VES_O1_NOTIFY_PNF_REGISTRATION_OUTPUT')
+    }
+
+}
