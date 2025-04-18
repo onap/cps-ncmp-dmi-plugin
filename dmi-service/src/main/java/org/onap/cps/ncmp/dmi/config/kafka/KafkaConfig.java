c756c37beb974448b8835aa4c527302930f0d981
mpriyank <priyank.maheshwari@est.tech>
2025-04-18 11:54:27 +0100

    [DMI] Automate the process of device registration #1
    - read from agreed topic using the VesEventSchema and call the
      registration endpoint in cps-ncmp to automate the device registration
      during mounting of the device.
    - also added non cloud kafka consumer factory as VES event is not cloud
      compliant
    - testware added
    
    Issue-ID: CPS-2710
    Change-Id: I5ab695afc225dcc372cff00a2f6f69c9047b14ed
    Signed-off-by: mpriyank <priyank.maheshwari@est.tech>

--- dmi-service/src/main/java/org/onap/cps/ncmp/dmi/config/kafka/KafkaConfig.java
+++ dmi-service/src/main/java/org/onap/cps/ncmp/dmi/config/kafka/KafkaConfig.java
@@ -1,6 +1,6 @@
 /*
  * ============LICENSE_START=======================================================
- * Copyright (C) 2023 Nordix Foundation
+ * Copyright (C) 2023-2025 OpenInfra Foundation Europe. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,6 +21,7 @@
 package org.onap.cps.ncmp.dmi.config.kafka;
 
 import io.cloudevents.CloudEvent;
+import java.time.Duration;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.producer.ProducerConfig;
@@ -136,6 +137,21 @@ public class KafkaConfig<T> {
         final ConcurrentKafkaListenerContainerFactory<String, CloudEvent> containerFactory =
             new ConcurrentKafkaListenerContainerFactory<>();
         containerFactory.setConsumerFactory(cloudEventConsumerFactory());
+        return containerFactory;
+    }
+
+    /**
+     * A legacy concurrent kafka listener container factory.
+     *
+     * @return instance of Concurrent kafka listener factory
+     */
+    @Bean
+    @Primary
+    public ConcurrentKafkaListenerContainerFactory<String, T> legacyEventConcurrentKafkaListenerContainerFactory() {
+        final ConcurrentKafkaListenerContainerFactory<String, T> containerFactory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        containerFactory.setConsumerFactory(legacyEventConsumerFactory());
+        containerFactory.getContainerProperties().setAuthExceptionRetryInterval(Duration.ofSeconds(10));
         return containerFactory;
     }
 
