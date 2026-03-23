b7fa16e9b4f86fc32fd3f145defbf14aca6f8bb2
seanbeirne <sean.beirne@est.tech>
2026-03-23 10:29:36 +0000

    Create ProvMnSExtensions interface or ONAP & STUB DMI
    IMPORTANT NOTE: Duplication will be resolved once api is finalised
    will be dependant on CPS repo similar to ProvMnS interface
    
    - New interface on ONAP DMI
    - New inferface on STUB DMI
    - ONAP DMI endpoint returns 501 NOT IMPLEMENTED
    - STUB DMI endpoint returns 200 OK
    
    Issue-ID: CPS-3177
    Change-Id: Icee9127e68a42470d93cdb0959c452d1347b827c
    Signed-off-by: seanbeirne <sean.beirne@est.tech>

---
+++ dmi-stub/dmi-stub-app/src/test/groovy/org/onap/cps/ncmp/dmi/rest/stub/controller/ProvMnsExtensionsStubControllerSpec.groovy
@@ -0,0 +1,71 @@
+/*
+ *  ============LICENSE_START=======================================================
+ *  Copyright (C) 2026 OpenInfra Foundation Europe. All rights reserved.
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.cps.ncmp.dmi.rest.stub.controller
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.onap.cps.ncmp.dmi.rest.stub.utils.ControllerSimulation
+import org.onap.cps.ncmp.dmi.rest.stub.utils.Sleeper
+import org.spockframework.spring.SpringBean
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.validation.Validator
+import spock.lang.Specification
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+
+@WebMvcTest([ProvMnSExtensionsStubController, ControllerSimulation])
+class ProvMnsExtensionsStubControllerSpec extends Specification {
+
+    @Autowired
+    MockMvc mockMvc
+
+    @SpringBean
+    Sleeper sleeper = Mock()
+
+    @Autowired
+    ObjectMapper objectMapper
+
+    @SpringBean
+    Validator validator = Mock()
+
+    def 'ProvMnS Extensions POST request with #scenario.'() {
+        given: 'a url'
+            def url = "/prov-mns-extensions/v1alpha1/actions/someSegment=1/otherSegment=2/${segmentName}=${segmentValue}/finalSegment"
+        when: 'post actions request is executed'
+            def response = mockMvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content('{"customProperties":{},"input":{}}'))
+                .andReturn().response
+        then: 'response status is #expectedHttpStatus'
+            assert response.status == expectedHttpStatus.value()
+        and: 'content contains the expected json (snippet)'
+            assert response.getContentAsString().contains(expectedContentSnippet)
+        where: 'following simulations are applied'
+            scenario        | segmentName      | segmentValue     || expectedHttpStatus       || expectedContentSnippet
+            'no simulation' | 'anotherSegment' | 'some value'     || HttpStatus.OK            || '"KeySetByStub":"ValueSetByStub"'
+            'delay 1'       | 'dmiSimulation'  | 'slowResponse_1' || HttpStatus.OK            || '"KeySetByStub":"ValueSetByStub"'
+            'http error'    | 'dmiSimulation'  | 'httpError_418'  || HttpStatus.I_AM_A_TEAPOT || '"status":"418"'
+    }
+
+}
